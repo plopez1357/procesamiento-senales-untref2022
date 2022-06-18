0771a1a64dc7adadfe0c5c32719c62d7a272604a
Pablo Lopez <pablo95lopez@gmail.com>
2022-06-17 21:21:48 -0300

    Avances TP7-Punto12

--- Entrega_practico/TP7-Punto12.py
+++ Entrega_practico/TP7-Punto12.py
@@ -10,27 +10,53 @@ en un factor de K (A1 = K A2).'''
 
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.signal.windows as wds
+from scipy.fft import fft, fftshift
 
+def generar_senoide(t, A, frec_Hz, φ):
+    return A * np.sin(2 * np.pi * frec_Hz * t + φ)
 
 def graficar():
-    T = 1 #Periodo
-    N = 5 #Cantidad de períodos
     inicio = 0
-    fin = 10 # El fin de la señal es la cantidad de períodos
+    fin = 2
     dur = fin - inicio #Duración
-    
-    frec_Hz_seno1 = 50;  
-    frec_Hz_seno2 = 51; 
-
     fs_Hz = 44100 #Frecuencia de muestreo
+    A_50 = 1 #amplitud
+    A_51 = 1*100
+    φ = 0 #Fase
 
     # Definir el rango "t".
-    t = np.linspace(inicio, fin, dur*fs, endpoint=None)
+    t = np.linspace(inicio, fin, dur*fs_Hz, endpoint=None)
 
-    #plt.plot(eje_frec, espectro_cuadrada, color='red', label='cuadrada')
-    #plt.plot(eje_frec, espectro_aperiodica, color='black', label='aperiodica')
+    senoide_50Hz = generar_senoide(t, A_50, 50, φ)
+    senoide_51Hz = generar_senoide(t, A_51, 51, φ)
+    suma_senoides = senoide_50Hz + senoide_51Hz
 
-    plt.legend()
+    ##
+    window = wds.boxcar(88200)
+    #plt.plot(window)
+    #plt.title("Boxcar window")
+   # plt.ylabel("Amplitude")
+   # plt.xlabel("Sample")
+
+    señal = suma_senoides * window
+
+    #plt.figure()
+    A = fft(señal, 2048, norm='forward') / (len(window)/2.0)
+    print(A)
+    freq = np.linspace(-0.5, 0.5, len(A))
+    response = 20 * np.log10(np.abs(fftshift(A / abs(A).max())))
+    plt.plot(freq, response)
+    plt.axis([-0.5, 0.5, -120, 0])
+    plt.title("Frequency response of the boxcar window")
+    plt.ylabel("Normalized magnitude [dB]")
+    plt.xlabel("Normalized frequency [cycles per sample]")
+
+    ##
+
+    #plt.plot(t, suma_senoides, color='red', label='cuadrada')
+
+    plt.legend(loc='upper right')
     plt.grid()
     plt.show()
 
