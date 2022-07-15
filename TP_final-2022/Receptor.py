b94bc97583a8b0c3b6e8d759d095abc7f5853a16
Hernan <h.d.fiorito@gmail.com>
2022-07-14 21:00:52 -0300

    Emprolijamiento de codigo

--- TP_final-2022/Receptor.py
+++ TP_final-2022/Receptor.py
@@ -1,8 +1,5 @@
-
 def demultiplexarSeñal(señalMultiplexada, cantCanales):
     señalesDemultiplexadas = []
-
     for i in range(cantCanales):
         señalesDemultiplexadas.append(señalMultiplexada[i::cantCanales])
-    
     return señalesDemultiplexadas
